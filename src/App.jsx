ed6133b472199f918c0823032972d11d8402f73f
Sandesh Shrestha <sandes.sth@gmail.com>
2023-05-18 20:53:42 -0700

    Initial commit: Create React app with Create Vite App

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Frontend development by Sandesh</h1>
+      
+        <section>
+          <h3>What is frontend web development?</h3>
+        <p>Front-end web development is the development of the graphical user interface of a website, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that website.
+        
+        </p>
+        </section>
+        <p>Here are some links</p>
+        <nav> 
+        <a href="https://www.facebook.com/"><button>facebook</button></a>
+        
+        <a href="https://github.com/"><button>github</button></a>
+        <br></br>
+        <a href="https://www.https://developer.mozilla.org/en-US/docs/Glossary/Semantics.com/"><button>HTML semantics</button></a>
+        <a href="https://https://gorakh.notion.site/Week-I-Introduction-to-Frontend-Development-with-HTML-CSS-JS-React-9106310088144180b5007eaaed54994e.com/"><button>In class notes</button></a>
+      </nav>
+        
+     
+    </>
+  )
+}
+
+export default App
